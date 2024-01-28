0a12230279c3a25ec3b9aa1ca46e258e1cc5f128
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-01-28 21:18:15 +0200

    Invert Binary Tree

---
+++ Grind100/InvertBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Grind100
+{
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    partial class Program
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return root;
+
+            TreeNode temp = root.left;
+            root.left = root.right;
+            root.right = temp;
+            InvertTree(root.left);
+            InvertTree(root.right);
+            return root;
+        }
+    }
+}
