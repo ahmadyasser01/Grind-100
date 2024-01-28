cd6304f62a44ed25a0cad8c4c56b1ce5650d869d
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-01-28 21:10:09 +0200

    Valid Parentheses problem

--- Grind100/Program.cs
+++ Grind100/Program.cs
@@ -6,6 +6,7 @@
         {
             //var answer = TwoSum([2, 7, 11, 15], 9);
             //var answer = MaxProfit([7, 6, 4, 3, 1]);
+            var answer = IsValid("(])");
 
         }
        
---
+++ Grind100/ValidParentheses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        static bool IsValid(string input)
+        {
+            Stack<char> stk = new Stack<char>();
+
+            foreach(var ch in input)
+            {
+                if(ch =='(' ||  ch =='[' || ch == '{')
+                {
+                    stk.Push(ch);
+                }
+                else
+                {
+                    
+                    if (!stk.TryPeek(out char result)) return false;
+                    if ((ch == ')' && result == '(') || (ch == '}' && result == '{') || (ch == ']' && result == '['))
+                    {
+                        stk.Pop();
+                    }
+                    else return false;
+                }
+            }
+            if(stk.Count == 0 ) return true;
+            return false;
+        }
+    }
+}
