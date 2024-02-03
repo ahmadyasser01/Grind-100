f685a23cbdee09c159febd68fc9aa7fa074343d2
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-02-03 23:13:24 +0200

    MergeTwoLists

---
+++ Grind100/MergeTwoSortedLists.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        } }
+        public partial class Program
+        {
+
+             static ListNode MergeTwoLists(ListNode list1, ListNode list2)
+            {
+                ListNode head = null;
+                ListNode tail = new ListNode(0,null);
+                ListNode f = list1;
+                ListNode s = list2;
+                while(f!=null && s!=null)
+                {
+
+                    if (f.val < s.val)
+                    {
+                        tail.next = f;
+                        f = f.next;
+                    }
+                    else
+                    {
+                        tail.next = s;
+                        s = s.next;
+                    }
+                    if (head == null) head = tail.next;
+                    tail = tail.next;
+                }
+                if (f != null) tail.next = f;
+                else tail.next = s;
+            if (head == null) head = tail.next;
+                return head;
+            }
+        }
+    }
+
--- Grind100/Program.cs
+++ Grind100/Program.cs
@@ -6,7 +6,17 @@
         {
             //var answer = TwoSum([2, 7, 11, 15], 9);
             //var answer = MaxProfit([7, 6, 4, 3, 1]);
-            var answer = IsValid("(])");
+            //var answer = IsValid("(])");
+            var list1_3 = new ListNode(4, null);
+            var list1_2 = new ListNode(2,list1_3);
+            var list1 = new ListNode(1, list1_2);
+
+            var list2_3 = new ListNode(4, null);
+            var list2_2 = new ListNode(3, list2_3);
+            var list2 = new ListNode(1, list2_2);
+
+
+            MergeTwoLists(null,new ListNode(0,null));
 
         }
        
