e62ef217b3addbc1decee9b2d0045f3d3ba7df2a
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-04 22:43:19 +0200

    BinarySearch

---
+++ Grind100/BinarySearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{ 
+    partial class Program
+    {
+        public int Search(int[] nums, int target)
+        {
+            int l = 0, r = nums.Length - 1;
+            int mid = (r - l) / 2 + l;
+            while (l <= r)
+            {
+                mid = (r - l) / 2 + l;
+                if (nums[mid] == target) { return mid; }
+                if (nums[mid] > target) r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+            return -1;
+        }
+    }
+}
