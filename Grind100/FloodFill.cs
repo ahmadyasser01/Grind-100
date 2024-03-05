4e5f411179e61bafd7b878d717890e291d4c524a
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-06 00:29:30 +0200

    flood fill

---
+++ Grind100/FloodFill.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public  static int[][] FloodFill(int[][] image, int sr, int sc, int color)
+        {
+            //save orig color
+            int origColor = image[sr][sc];
+
+            // create Queue
+            Queue<int> row = new Queue<int>();
+            Queue<int> col = new Queue<int>();
+            bool[,] visited = new bool[image.Length, image[0].Length];
+            row.Enqueue(sr);
+            col.Enqueue(sc);
+            
+            while(row.Count > 0)
+            {
+                int r = row.Dequeue();
+                int c = col.Dequeue();
+                visited[r,c] = true;
+                if (image[r][c] == origColor) image[r][c] = color;
+
+                if (c - 1 >= 0 && !visited[r, c - 1] && image[r][c - 1] == origColor)
+                {
+                    row.Enqueue(r);
+                    col.Enqueue(c - 1);
+                }
+                if (r-1>=0  && !visited[r - 1,c] && image[r-1][c] == origColor)
+                {
+                    row.Enqueue(r - 1);
+                    col.Enqueue(c);
+                }
+
+                if (r + 1 < image.Length && !visited[r + 1, c] && image[r+1][c] == origColor)
+                {
+                    row.Enqueue(r + 1);
+                    col.Enqueue(c);
+                }
+               
+                if (c +1 < image[0].Length && !visited[r, c+1] && image[r][c+1] == origColor)
+                {
+                    row.Enqueue(r);
+                    col.Enqueue(c +1);
+                }
+            }
+
+            return image;
+
+        }
+    }
+}
--- Grind100/Program.cs
+++ Grind100/Program.cs
@@ -7,18 +7,24 @@
             //var answer = TwoSum([2, 7, 11, 15], 9);
             //var answer = MaxProfit([7, 6, 4, 3, 1]);
             //var answer = IsValid("(])");
-            var list1_3 = new ListNode(4, null);
-            var list1_2 = new ListNode(2,list1_3);
-            var list1 = new ListNode(1, list1_2);
+            //var list1_3 = new ListNode(4, null);
+            //var list1_2 = new ListNode(2,list1_3);
+            //var list1 = new ListNode(1, list1_2);
 
-            var list2_3 = new ListNode(4, null);
-            var list2_2 = new ListNode(3, list2_3);
-            var list2 = new ListNode(1, list2_2);
+            //var list2_3 = new ListNode(4, null);
+            //var list2_2 = new ListNode(3, list2_3);
+            //var list2 = new ListNode(1, list2_2);
 
 
             //MergeTwoLists(null,new ListNode(0,null));
 
-            IsPalindrome("A man, a plan, a canal: Panama");
+            //IsPalindrome("A man, a plan, a canal: Panama");
+            int[][] input = new int[3][] { 
+            new int[]{1,1,1},
+            new int[]{1,1,0},
+            new int[]{1,0,1},
+            };
+            var result = FloodFill(input,1,1,2);
 
         }
        
