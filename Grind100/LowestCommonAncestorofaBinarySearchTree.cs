228de54c5b604972f02b99bf37faf5e17c2dedb8
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-07 00:57:23 +0200

    Lowest Common Ancestor of a Binary Search Tree

---
+++ Grind100/LowestCommonAncestorofaBinarySearchTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            while (root is not null)
+            {
+                if (p.val > root.val && q.val > root.val)
+                    root = root.right;
+                else if (p.val < root.val && q.val < root.val)
+                    root = root.left;
+                else
+                    return root;
+            }
+            return root;
+        }
+    }
+}
