e4a2a8fb2dbb23b21b511f57c37883403be0d53a
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-10 00:19:59 +0200

    BalancedBinarySearchTree

---
+++ Grind100/BalancedBinarySearchTree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            if (root is null) return true;
+            int l = geth(root.left);
+            int r = geth(root.right);
+            if (Math.Abs(l - r) > 1) return false;
+            return IsBalanced(root.left) && IsBalanced(root.right);
+        }
+        public int geth(TreeNode root)
+        {
+            if (root is null) return 0;
+            return 1 + Math.Max(geth(root.left), geth(root.right));
+        }
+    }
+}
