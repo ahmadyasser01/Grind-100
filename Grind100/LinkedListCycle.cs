46e480d05051f7041b29109f22e48d9dd3f8d1cb
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-09 21:40:28 +0200

    LinkedList Cycle

---
+++ Grind100/LinkedListCycle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+
+
+    partial class Program
+    {
+        public bool HasCycle(ListNode head)
+        {
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast is not null && fast.next is not null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+                if (fast == slow) return true;
+
+            }
+            return false;
+        }
+    }
+}
