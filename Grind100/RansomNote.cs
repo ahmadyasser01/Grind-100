a185f87f51b1fc4a0eebfaa0a3f4211cf2c13003
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-10 00:18:25 +0200

    RansomNote

---
+++ Grind100/RansomNote.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            char[] arr = new char[26];
+            foreach (var ch in magazine)
+            {
+                arr[ch - 'a']++;
+            }
+            foreach (var ch in ransomNote)
+            {
+                if (arr[ch - 'a'] <= 0) return false;
+                arr[ch - 'a']--;
+
+            }
+            foreach (var ch in arr)
+            {
+                if (ch < 0) return false;
+            }
+            return true;
+        }
+    }
+}
