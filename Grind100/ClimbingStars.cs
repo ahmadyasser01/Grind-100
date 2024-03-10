fd033e4bac13cf1f4ad0a06f5d23a10a511d18f8
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-10 19:55:22 +0200

    climbing stars

---
+++ Grind100/ClimbingStars.cs
@@ -0,0 +1,21 @@
+﻿
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public int ClimbStairs(int n)
+        {
+            int a = 1;
+            int b = 2;
+            int c = n == 1 ? 1 : n == 2 ? 2 : 3;
+            for (int i = 3; i <= n; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+    }
+}
