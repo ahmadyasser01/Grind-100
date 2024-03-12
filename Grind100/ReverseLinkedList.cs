45292d2f5be18f64c10e2ab30ec63f73418fbde2
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-12 03:17:05 +0200

    reverse Linked List

---
+++ Grind100/ReverseLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public ListNode ReverseList(ListNode head)
+        {
+            ListNode r = null;
+            ListNode q = head;
+            ListNode p = head;
+            if (p is not null) p = p.next;
+            while (p is not null)
+            {
+
+                q.next = r;
+                r = q;
+                q = p;
+                if (p is not null)
+                    p = p.next;
+            }
+            if (q is not null)
+                q.next = r;
+            return q;
+        }
+        }
+    }
+}
