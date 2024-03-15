c95531a50f6f16945d7e7f60d907304d356c2f8e
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-15 16:31:42 +0200

    ContainsDuplicate

---
+++ Grind100/ContainsDuplicate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public bool ContainsDuplicate(int[] nums)
+        { 
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == nums[i + 1]) return true;
+            }
+            return false;
+        }
+    }
+}
