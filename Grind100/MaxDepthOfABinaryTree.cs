bb6d62d8e9a1c6740399cdd3f1b9a62e4700c142
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-15 16:35:28 +0200

    MaxDepthOfABinaryTree

---
+++ Grind100/MaxDepthOfABinaryTree.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+            return Math.Max(MaxDepth(root.left), MaxDepth(root.right)) + 1;
+        }
+    }
+}
