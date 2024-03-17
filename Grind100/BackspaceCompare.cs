f381f1744a0ff776b82a9fae098c6d76371a5fc9
ahmadyasser01 <ahmadyasser2814@gmail.com>
2024-03-17 17:13:29 +0200

    BackspaceCompare

---
+++ Grind100/BackspaceCompare.cs
@@ -0,0 +1,34 @@
+﻿
+
+using System.Text;
+
+namespace Grind100
+{
+    partial class Program
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            StringBuilder sb_S = new StringBuilder();
+            StringBuilder sb_T = new StringBuilder();
+
+            foreach (var chr in s)
+            {
+                if (chr == '#')
+                {
+                    if (sb_S.Length > 0) sb_S.Remove(sb_S.Length - 1, 1);
+                }
+                else sb_S.Append(chr);
+            }
+            foreach (var chr in t)
+            {
+                if (chr == '#')
+                {
+                    if (sb_T.Length > 0) sb_T.Remove(sb_T.Length - 1, 1);
+                }
+                else sb_T.Append(chr);
+            }
+            return string.Compare(sb_S.ToString(), sb_T.ToString()) == 0 ? true : false;
+
+        }
+    }
+}
